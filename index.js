23e80a29e46bc4963883be28541683551410846d
Andrew Smithson <smithson@uk.ibm.com>
2016-12-19 14:06:48 +0000

    Prompt for connection credentials

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 #!/usr/bin/env node
-console.log("zosjobs");
+
+'use strict';
+var inquirer = require('inquirer');
+
+var loginDetails = [
+    {
+        type: 'input',
+        name: 'connUrl',
+        message: 'z/OS MF URL',
+    },
+    {
+        type: 'input',
+        name: 'user',
+        message: 'User ID'
+    },
+    {
+        type: 'password',
+        name: 'password',
+        message: 'Password'
+    }
+];
+
+inquirer.prompt(loginDetails).then(function(answers){
+    console.log("\nLogin details:");
+    console.log(JSON.stringify(answers, null, '   '));
+});
