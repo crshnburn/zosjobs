1575d88f75c8586590af2f0f4982d1c1d6d9f198
Andrew Smithson <smithson@uk.ibm.com>
2016-12-20 16:06:58 +0000

    Complete error checking code

--- index.js
+++ index.js
@@ -4,25 +4,21 @@
 var inquirer = require('inquirer');
 var request = require('request');
 
-var loginDetails = [
-    {
-        type: 'input',
-        name: 'connUrl',
-        message: 'z/OS MF URL',
-    },
-    {
-        type: 'input',
-        name: 'user',
-        message: 'User ID'
-    },
-    {
-        type: 'password',
-        name: 'password',
-        message: 'Password'
-    }
-];
+var loginDetails = [{
+    type: 'input',
+    name: 'connUrl',
+    message: 'z/OS MF URL',
+}, {
+    type: 'input',
+    name: 'user',
+    message: 'User ID'
+}, {
+    type: 'password',
+    name: 'password',
+    message: 'Password'
+}];
 
-inquirer.prompt(loginDetails).then(function(answers){
+inquirer.prompt(loginDetails).then(function (answers) {
     var connUrl = answers.connUrl;
     var options = {
         uri: answers.connUrl + '/zosmf/restjobs/jobs',
@@ -34,58 +30,76 @@ inquirer.prompt(loginDetails).then(function(answers){
         json: true,
         strictSSL: false
     };
-    request.get(options, function(error, response, data){
-        if(error){
+    request.get(options, function (error, response, data) {
+        if (error) {
             console.log(error);
             process.exit(1);
         } else if (response.statusCode != 200) {
-            if(data === undefined){
+            if (data === undefined) {
                 console.log(response.statusMessage);
             } else {
                 console.log(data);
             }
         } else {
-        var jobs = {};
-        
-        data.forEach(function(job){
-            jobs[job.jobname] = job;
-        });
-        var joblist = [
-            {
+            var jobs = {};
+
+            data.forEach(function (job) {
+                jobs[job.jobname] = job;
+            });
+            var joblist = [{
                 type: 'list',
                 name: 'jobname',
                 message: 'Which Job?',
                 choices: Object.keys(jobs)
-            }
-        ];
-        inquirer.prompt(joblist).then(function(answers){
-            console.log("Get details for JOBID " + answers.jobname);
-            options.uri = jobs[answers.jobname].url;
-            request.get(options, function(error, response, data){
-                options.uri = data['files-url'];
-                request.get(options, function(error, response, data){
-                    var ddCards = {};
-                    data.forEach(function(dd){
-                        ddCards[dd.ddname] = dd;
-                    });
-                    var ddlist = [
-                        {
-                            type: 'list',
-                            name: 'ddcard',
-                            message: 'Which DD card?',
-                            choices: Object.keys(ddCards)
+            }];
+            inquirer.prompt(joblist).then(function (answers) {
+                console.log("Get details for JOBID " + answers.jobname);
+                options.uri = jobs[answers.jobname].url;
+                request.get(options, function (error, response, data) {
+                    options.uri = data['files-url'];
+                    request.get(options, function (error, response, data) {
+                        if (error) {
+                            console.log(error);
+                            process.exit(1);
+                        } else if (response.statusCode != 200) {
+                            if (data === undefined) {
+                                console.log(response.statusMessage);
+                            } else {
+                                console.log(data);
+                            }
+                        } else {
+                            var ddCards = {};
+                            data.forEach(function (dd) {
+                                ddCards[dd.ddname] = dd;
+                            });
+                            var ddlist = [{
+                                type: 'list',
+                                name: 'ddcard',
+                                message: 'Which DD card?',
+                                choices: Object.keys(ddCards)
+                            }];
+                            inquirer.prompt(ddlist).then(function (answers) {
+                                options.uri = ddCards[answers.ddcard]['records-url'];
+                                options.json = false;
+                                request.get(options, function (error, response, data) {
+                                    if (error) {
+                                        console.log(error);
+                                        process.exit(1);
+                                    } else if (response.statusCode != 200) {
+                                        if (data === undefined) {
+                                            console.log(response.statusMessage);
+                                        } else {
+                                            console.log(data);
+                                        }
+                                    } else {
+                                        console.log(data);
+                                    }
+                                });
+                            });
                         }
-                    ];
-                    inquirer.prompt(ddlist).then(function(answers){
-                        options.uri = ddCards[answers.ddcard]['records-url'];
-                        options.json = false;
-                        request.get(options, function(error, response, data){
-                            console.log(data);
-                        });
                     });
                 });
             });
-        });
         }
     });
 });
