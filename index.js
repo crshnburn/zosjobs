ed5a81c456f19968c56c8a6b25fca6ed39be7478
Andrew Smithson <smithson@uk.ibm.com>
2016-12-20 15:22:35 +0000

    Display list of dd cards and retrieve contents

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ var loginDetails = [
 ];
 
 inquirer.prompt(loginDetails).then(function(answers){
+    var connUrl = answers.connUrl;
     var options = {
         uri: answers.connUrl + '/zosmf/restjobs/jobs',
         auth: {
@@ -34,6 +35,46 @@ inquirer.prompt(loginDetails).then(function(answers){
         strictSSL: false
     };
     request.get(options, function(error, response, data){
-        console.log(JSON.stringify(data, null, 3));
+        var jobs = {};
+        
+        data.forEach(function(job){
+            jobs[job.jobname] = job;
+        });
+        var joblist = [
+            {
+                type: 'list',
+                name: 'jobname',
+                message: 'Which Job?',
+                choices: Object.keys(jobs)
+            }
+        ];
+        inquirer.prompt(joblist).then(function(answers){
+            console.log("Get details for JOBID " + answers.jobname);
+            options.uri = jobs[answers.jobname].url;
+            request.get(options, function(error, response, data){
+                options.uri = data['files-url'];
+                request.get(options, function(error, response, data){
+                    var ddCards = {};
+                    data.forEach(function(dd){
+                        ddCards[dd.ddname] = dd;
+                    });
+                    var ddlist = [
+                        {
+                            type: 'list',
+                            name: 'ddcard',
+                            message: 'Which DD card?',
+                            choices: Object.keys(ddCards)
+                        }
+                    ];
+                    inquirer.prompt(ddlist).then(function(answers){
+                        options.uri = ddCards[answers.ddcard]['records-url'];
+                        options.json = false;
+                        request.get(options, function(error, response, data){
+                            console.log(data);
+                        });
+                    });
+                });
+            });
+        });
     });
 });
