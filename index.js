41833e3e17de0eada3a23da2ae8ff6b7e9c33f10
Andrew Smithson <smithson@uk.ibm.com>
2017-08-14 16:37:37 +0100

    Refactor to commands and add add-server command

--- index.js
+++ index.js
@@ -18,154 +18,160 @@
 
 /* istanbul ignore next */
 
-const inquirer = require('inquirer');
+// const inquirer = require('inquirer');
 const program = require('commander');
-const async = require('async');
+// const async = require('async');
 
-const ZosJobs = require('./zosjobs.js');
+// const ZosJobs = require('./zosjobs.js');
+const packageInfo = require('./package.json');
 
-program.version('0.1.0')
-  .option('-c, --connUrl [url]', 'specifies the z/OS MF hostname and port')
-  .option('-u, --user [user]', 'specifies the user ID')
-  .option('-p, --password [password]', 'specifies the password')
-  .option('-o, --owner [owner]', 'the owner of the jobs to retrieve')
+program.version(packageInfo.version)
+  .command('add-server', 'Add a z/OS MF Server configuration')
+  .alias('add')
+  .command('update-server', 'Update a z/OS MF Server configuration')
+  .alias('update')
+  .command('delete-server', 'Remove a z/OS MF Server configuration')
+  .alias('delete')
+  .command('jobs', 'Get information about a z/OS Job')
+  .command('submit', 'Submit a Job')
+  .command('command', 'Issue a command')
   .parse(process.argv);
 
-let connUrl;
-let user;
-let password;
-let owner;
+// let connUrl;
+// let user;
+// let password;
+// let owner;
 
-async.series([
-  function connUrlPrompt(done) {
-    if (program.connUrl) {
-      connUrl = program.connUrl;
-      done();
-    } else {
-      inquirer.prompt([{
-        type: 'input',
-        name: 'connUrl',
-        message: 'z/OS MF URL',
-      }]).then((answers) => {
-        connUrl = answers.connUrl;
-        done();
-      });
-    }
-  },
-  function userPrompt(done) {
-    if (program.user) {
-      user = program.user;
-      done();
-    } else {
-      inquirer.prompt([{
-        type: 'input',
-        name: 'user',
-        message: 'User ID',
-      }]).then((answers) => {
-        user = answers.user;
-        done();
-      });
-    }
-  },
-  function passwordPrompt(done) {
-    if (program.password) {
-      password = program.password;
-      done();
-    } else {
-      inquirer.prompt([{
-        type: 'password',
-        name: 'password',
-        message: 'Password',
-      }]).then((answers) => {
-        password = answers.password;
-        done();
-      });
-    }
-  },
-], () => {
-  const zosjobs = new ZosJobs(connUrl, user, password, owner);
-  const zosjobsOptions = [{
-    type: 'list',
-    name: 'options',
-    message: 'What do you want to do?',
-    choices: ['List Jobs', 'Submit Job', 'Issue Command'],
-  }];
-  inquirer.prompt(zosjobsOptions).then((listAnswers) => {
-    if (listAnswers.options === 'List Jobs') {
-      async.series([
-        function getOwner(done) {
-          if (program.owner) {
-            owner = program.owner;
-            done();
-          } else {
-            inquirer.prompt([{
-              type: 'input',
-              name: 'owner',
-              message: 'Owner',
-              default: user,
-            }]).then((answers) => {
-              owner = answers.owner;
-              done();
-            });
-          }
-        },
-        function getJobs(done) {
-          zosjobs.getJobs(owner).then((jobs) => {
-            if (Object.keys(jobs).length > 0) {
-              const joblist = [{
-                type: 'list',
-                name: 'jobname',
-                message: 'Which Job?',
-                choices: Object.keys(jobs),
-              }];
-              inquirer.prompt(joblist).then((answers) => {
-                zosjobs.getJobCards(jobs[answers.jobname]).then((ddCards) => {
-                  if (Object.keys(ddCards).length > 0) {
-                    const ddlist = [{
-                      type: 'list',
-                      name: 'ddcard',
-                      message: 'Which DD card?',
-                      choices: Object.keys(ddCards),
-                    }];
-                    inquirer.prompt(ddlist).then((ddanswers) => {
-                      zosjobs.getRecords(ddCards[ddanswers.ddcard])
-                        .then(data => console.log(data)).catch(error => console.log(error));
-                    });
-                  } else {
-                    console.log('No DD Cards for Job');
-                  }
-                }).catch(error => console.log(error));
-              });
-            } else {
-              console.log('No Jobs Found');
-            }
-          }).catch(error => console.log(error));
-          done();
-        },
-      ]);
-    } else if (listAnswers.options === 'Submit Job') {
-      inquirer.prompt([{
-        type: 'editor',
-        name: 'jcl',
-        message: 'Enter JCL:',
-      }]).then((answers) => {
-        // console.log(answers);
-        zosjobs.submitJob(answers.jcl).then(console.log).catch(console.log);
-      });
-    } else {
-      const commandOptions = [{
-        type: 'input',
-        name: 'command',
-        message: 'Enter the command',
-      },
-      {
-        type: 'input',
-        name: 'system',
-        message: 'Which system should the command run on?',
-      }];
-      inquirer.prompt(commandOptions).then((answers) => {
-        zosjobs.issueCommand(answers.command, answers.system).then(console.log).catch(console.log);
-      });
-    }
-  });
-});
+// async.series([
+//   function connUrlPrompt(done) {
+//     if (program.connUrl) {
+//       connUrl = program.connUrl;
+//       done();
+//     } else {
+//       inquirer.prompt([{
+//         type: 'input',
+//         name: 'connUrl',
+//         message: 'z/OS MF URL',
+//       }]).then((answers) => {
+//         connUrl = answers.connUrl;
+//         done();
+//       });
+//     }
+//   },
+//   function userPrompt(done) {
+//     if (program.user) {
+//       user = program.user;
+//       done();
+//     } else {
+//       inquirer.prompt([{
+//         type: 'input',
+//         name: 'user',
+//         message: 'User ID',
+//       }]).then((answers) => {
+//         user = answers.user;
+//         done();
+//       });
+//     }
+//   },
+//   function passwordPrompt(done) {
+//     if (program.password) {
+//       password = program.password;
+//       done();
+//     } else {
+//       inquirer.prompt([{
+//         type: 'password',
+//         name: 'password',
+//         message: 'Password',
+//       }]).then((answers) => {
+//         password = answers.password;
+//         done();
+//       });
+//     }
+//   },
+// ], () => {
+//   const zosjobs = new ZosJobs(connUrl, user, password, owner);
+//   const zosjobsOptions = [{
+//     type: 'list',
+//     name: 'options',
+//     message: 'What do you want to do?',
+//     choices: ['List Jobs', 'Submit Job', 'Issue Command'],
+//   }];
+//   inquirer.prompt(zosjobsOptions).then((listAnswers) => {
+//     if (listAnswers.options === 'List Jobs') {
+//       async.series([
+//         function getOwner(done) {
+//           if (program.owner) {
+//             owner = program.owner;
+//             done();
+//           } else {
+//             inquirer.prompt([{
+//               type: 'input',
+//               name: 'owner',
+//               message: 'Owner',
+//               default: user,
+//             }]).then((answers) => {
+//               owner = answers.owner;
+//               done();
+//             });
+//           }
+//         },
+//         function getJobs(done) {
+//           zosjobs.getJobs(owner).then((jobs) => {
+//             if (Object.keys(jobs).length > 0) {
+//               const joblist = [{
+//                 type: 'list',
+//                 name: 'jobname',
+//                 message: 'Which Job?',
+//                 choices: Object.keys(jobs),
+//               }];
+//               inquirer.prompt(joblist).then((answers) => {
+//                 zosjobs.getJobCards(jobs[answers.jobname]).then((ddCards) => {
+//                   if (Object.keys(ddCards).length > 0) {
+//                     const ddlist = [{
+//                       type: 'list',
+//                       name: 'ddcard',
+//                       message: 'Which DD card?',
+//                       choices: Object.keys(ddCards),
+//                     }];
+//                     inquirer.prompt(ddlist).then((ddanswers) => {
+//                       zosjobs.getRecords(ddCards[ddanswers.ddcard])
+//                         .then(data => console.log(data)).catch(error => console.log(error));
+//                     });
+//                   } else {
+//                     console.log('No DD Cards for Job');
+//                   }
+//                 }).catch(error => console.log(error));
+//               });
+//             } else {
+//               console.log('No Jobs Found');
+//             }
+//           }).catch(error => console.log(error));
+//           done();
+//         },
+//       ]);
+//     } else if (listAnswers.options === 'Submit Job') {
+//       inquirer.prompt([{
+//         type: 'editor',
+//         name: 'jcl',
+//         message: 'Enter JCL:',
+//       }]).then((answers) => {
+//         // console.log(answers);
+//         zosjobs.submitJob(answers.jcl).then(console.log).catch(console.log);
+//       });
+//     } else {
+//       const commandOptions = [{
+//         type: 'input',
+//         name: 'command',
+//         message: 'Enter the command',
+//       },
+//       {
+//         type: 'input',
+//         name: 'system',
+//         message: 'Which system should the command run on?',
+//       }];
+//       inquirer.prompt(commandOptions).then((answers) => {
+//         zosjobs.issueCommand(answers.command, answers.system).then(console.log).catch(console.log);
+//       });
+//     }
+//   });
+// });
---
+++ zosjobs-add-server.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+console.log('add-server command');
+
+const inquirer = require('inquirer');
+const program = require('commander');
+const async = require('async');
+const Configstore = require('configstore');
+const pkg = require('./package.json');
+
+// create a Configstore instance with an unique ID e.g. 
+// Package name and optionally some default values 
+const conf = new Configstore(pkg.name);
+
+program.parse(process.argv);
+
+let name;
+let connUrl;
+let userName;
+let pass;
+
+async.series([
+  function namePrompt(done) {
+    inquirer.prompt([{
+      type: 'input',
+      name: 'name',
+      message: 'Server Name',
+    }]).then((answers) => {
+      name = answers.name;
+      done();
+    });
+  },
+  function connUrlPrompt(done) {
+    inquirer.prompt([{
+      type: 'input',
+      name: 'connUrl',
+      message: 'z/OS MF URL',
+    }]).then((answers) => {
+      connUrl = answers.connUrl;
+      done();
+    });
+  },
+  function userPrompt(done) {
+    inquirer.prompt([{
+      type: 'input',
+      name: 'user',
+      message: 'User ID',
+    }]).then((answers) => {
+      userName = answers.user;
+      done();
+    });
+  },
+  function passwordPrompt(done) {
+    inquirer.prompt([{
+      type: 'password',
+      name: 'password',
+      message: 'Password',
+    }]).then((answers) => {
+      pass = answers.password;
+      done();
+    });
+  },
+], () => {
+  conf.set(name, {
+    url: connUrl,
+    user: userName,
+    password: pass,
+  });
+});
